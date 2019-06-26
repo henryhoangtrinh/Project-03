8a75633cbbc308e42127f0471d54be579e1eee3c
Hoang Trinh <henryhoangtrinh@gmail.com>
2019-06-26 13:23:37 -0500

    initial the project

---
+++ AboutTime/AboutTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutTime
+//
+//  Created by Henry Trinh on 6/26/19.
+//  Copyright © 2019 HR-Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
